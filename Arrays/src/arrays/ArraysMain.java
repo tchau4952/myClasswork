e3afbe5c9e640e044320f893a3768f1c9cfa6a21
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-10-18 14:35:48 -0400

    10/18

--- Arrays/src/arrays/ArraysMain.java
+++ Arrays/src/arrays/ArraysMain.java
@@ -10,14 +10,48 @@ public class ArraysMain {
 	private int[] testArray;
 	
 	public ArraysMain() {
-		int[] arr = {1,2,3,4,5,6};
-		longestConsecutiveSequence(arr);
+		wednesdayMethods();
 	}
-	private void tuesdayMethods() {
-		int[] orderTest = {1,2,3,4,5,6,7,8,9,10};
-//		cycleThrough(orderTest,5);
-//		System.out.println(Arrays.toString(orderTest));
-		System.out.println(longestConsecutiveSequence(orderTest) + " is the l.c.s. it should be 6");
+	private void wednesdayMethods() {
+		int[] diceRolls = new int[10000];
+		populate(diceRolls);
+		int[] data = longestConsecutiveSeqAndIndex(diceRolls);
+		int longest = data[0];
+		System.out.println("The longest sequence is " + longest + "rolls."
+				+ " It happened on roll # " +data[1]+ " the sequence was: "
+				+ Arrays.toString(subArray(diceRolls, 
+						data[1], data[0])) + ".");
+	}
+	
+	/**
+	 * BIG IDEA:
+	 * Usually a method returns ONE piece of data (i.e. 'int', 'boolean', etc)
+	 * IF we ever want more than one piece of data, one way of doing it
+	 * is by using an array, as you see here, a method that returns the LENGTH
+	 * of the sequence and its START position (both ints)
+	 * @param arr
+	 * @return
+	 */
+	
+	private int[] longestConsecutiveSeqAndIndex(int[] arr) {
+		//use an int[] to store the data
+			int[] data = new int[2];//element at zero is length, at 1 is position
+				
+			data[0] = 1;
+			int currentCount = 1;
+			for(int i = 0; i < arr.length; i++) {
+				while(i + currentCount < arr.length && isConsecutive(arr, i, i + currentCount)) {
+					currentCount++;
+				}
+				if(currentCount > data[0]) {
+					data[0] = currentCount;
+					//also store the index where this sequence started
+					data[1] = i;
+				}
+				i = i + currentCount - 1;//saves time
+				currentCount = 1;
+			}
+			return data;
 	}
 	/**
 	 * return the length of the longest consecutive array
@@ -42,6 +76,13 @@ public class ArraysMain {
 			currentCount = 1;
 		}
 		return maxLength;
+	}
+	
+	private void tuesdayMethods() {
+		int[] orderTest = {1,2,3,4,5,1,6,7,8,9,10,11};
+//		cycleThrough(orderTest,5);
+//		System.out.println(Arrays.toString(orderTest));
+		System.out.println(longestConsecutiveSequence(orderTest) + " is the l.c.s. it should be 6");
 	}
 	
 	/**
