eee090104c4a407e10d3b231c59662926f055a3d
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-10-24 15:32:18 -0400

    10/24

--- Arrays/src/caveExplorer/CaveRoom.java
+++ Arrays/src/caveExplorer/CaveRoom.java
@@ -44,8 +44,8 @@ public class CaveRoom {
 		for(int i = 0; i < doors.length; i++) {
 			if(doors[i] != null) {
 				doorFound = true;
-				directions += "There is a "+doors[i]getDescription()+" to the "+
-				toDirection(i)+". "+doors[i].getDetails();
+				directions += "There is a "+doors[i].getDescription()+" to the "+
+				toDirection(i)+". "+doors[i].getDetails()+"\n";
 			}
 		}
 		if(!doorFound) {
@@ -60,15 +60,99 @@ public class CaveRoom {
 	 * converts an int to a direction:
 	 * 	0 - > "the North"
 	 *  1 -> "the South"
-	 *  hint: Complete this method without usingan if statement
+	 *  hint: Complete this method without using an if statement
 	 * @param dir
 	 * @return
 	 */
+	
 	public static String toDirection (int dir) {
 		String[] directions = {"the North", "the East", "the South", "the West"};
 		//NOTE: when I say "no long if-else" statements,
 		//this is how you should be thinking
 		return directions[dir];
+	}
+	
+	public void enter() {
+		contents = "X";
+	}
+	
+	public void leave() {
+		contents = defaultContents;
+	}
+	
+	/**
+	 * This is how we join rooms together.
+	 * It gives this room access to anotherRoom and vice-versa
+	 * It also puts the door between both rooms
+	 * @param direction
+	 * @param anotherRoom
+	 * @param door
+	 */
+	public void setConnection(int direction, CaveRoom anotherRoom, Door door) {
+		addRoom(direction, anotherRoom, door);
+		anotherRoom(oppositeDirection(direction), this, door);
+	}
+	
+	private void addRoom(int dir, CaveRoom caveRoom, Door door) {
+		borderingRooms[dir] = caveRoom;
+		doors[dir] = door;
+		setDirections();//updates the directions
+	}
+
+	public void interpretInput(String input) {
+		while(!isValid(input)) {
+			System.out.println("You can only enter 'w', 'a', 's', or 'd'.");
+			input = CaveExplorer.in.nextLine();
+		}
+		int direction = "wdsa".indexOf(input);
+		/*
+		 * convert w,a,s,d to directions 0,3,2,1
+		 * NO IF STATEMENTS
+		 */
+		goToRoom(direction);
+	}
+	
+	/**
+	 * returns true if w,a,s, or d is the input (NO IF STATEMENTS)
+	 * @param input
+	 * @return
+	 */
+	private boolean isValid(String input) {
+	//	String inputChars = "wdsa";
+	//	return inputChars.indexOf(input) != -1;
+		return "wasd".indexOf(input) > -1 && input.length() == -1;
+	}
+	
+	/**
+	 * THIS IS WHERE YOU EDIT YOUR CAVES
+	 */
+	public static void setUpCaves() {
+		
+	}
+
+	public void goToRoom(int direction) {
+		//make sure there is a room to go to:
+		if(borderingRooms[direction] != null && doors[direction] != null &&
+				doors[direction].isOpen()) {
+			CaveExplorer.currentRoom.leave();
+			CaveExplorer.currentRoom = borderingRooms[direction];
+			CaveExplorer.currentRoom.enter();
+			CaveExplorer.inventory.updateMap();
+		}else {
+			//print red text
+			System.err.println("You can't do that!");
+		}
+	}
+	
+	/**
+	 * returns the OPPOSITE direction
+	 * 	oD(0) returns 2
+	 * 	oD(1) returns 3
+	 * @param dir
+	 * @return
+	 */
+	public static int oppositeDirection(int dir) {
+		return (dir+2)%4;
 	}
 	
 	private void setDefaultContents(String defaultContents) {
