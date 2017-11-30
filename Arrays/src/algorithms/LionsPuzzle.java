bd533d241f5a52fa5ddadefdb2650e2ca15b6ca2
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-11-30 15:06:20 -0500

    yay

---
+++ Arrays/src/algorithms/LionsPuzzle.java
@@ -0,0 +1,22 @@
+package algorithms;
+
+public class LionsPuzzle {
+
+	public static void main(String[] args) {
+		int numberOfLions = 101;
+		if(willEat(numberOfLions)) {
+			System.out.println("Eat the sheep!");
+		}else {
+			System.out.println("Do not eat the sheep!");
+		}
+	}
+
+	private static boolean willEat(int numberOfLions) {
+		if(numberOfLions == 1) {
+			return true;
+		}
+		return false;
+	}
+
+	
+}
